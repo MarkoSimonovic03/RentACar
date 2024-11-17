4290e4e17866b4cff47e82194caff11f09d43a5a
MarkoSimonovic03 <max2001car@gmail.com>
2024-11-17 18:17:16 +0100

    Add project files.

---
+++ RentACar/Controllers/BookingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACar.Models;
+
+namespace RentACar.Controllers
+{
+	public class BookingController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Booking(Car carId)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Booking(Booking booking)
+		{
+			return View();
+		}
+	}
+}
---
+++ RentACar/Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace RentACar.Models
+{
+	public class Booking
+	{
+		public int BookingId { get; set; }
+		public int CarId { get; set; }
+		public int UserId { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public decimal TotalPrice { get; set; }
+	}
+}
